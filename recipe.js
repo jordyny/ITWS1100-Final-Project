c3717b0bd841eeb1e5c0ad2754494145b6cd969d
caitlinyau0717 <yauc2@rpi.edu>
2024-04-08 22:03:08 -0400

    recipe button

--- recipe.js
+++ recipe.js
@@ -1,13 +1,18 @@
-var breakfastButton = document.getElementById('breakfast');
-var lunchButton = document.getElementById('lunch');
-var dinnerButton = document.getElementById('dinner');
-var snackButton = document.getElementById('snack');
-
 function selectedMeal(mealType) {
    document.getElementById("selected-meal").textContent = "Selected Meal: " + mealType;
-   if (mealType == "breakfast") {
-      document.getElementById("button").bgcolor="#000000";
-   }
+   document.getElementById('breakfast').style = 'background-color: #fdf5df'
+   document.getElementById('lunch').style = 'background-color: #fdf5df'
+   document.getElementById('dinner').style = 'background-color: #fdf5df'
+   document.getElementById('dessert').style = 'background-color: #fdf5df'
+   if (mealType == "Breakfast") {
+      document.getElementById('breakfast').style = 'background-color: #ffd5ff;';
+   } else if (mealType == "Lunch") {
+      document.getElementById('lunch').style = 'background-color: #ffd5ff;';
+   } else if (mealType == "Dinner") {
+      document.getElementById('dinner').style = 'background-color: #ffd5ff;';
+   } else if (mealType == "Dessert") {
+      document.getElementById('dessert').style = 'background-color: #ffd5ff;';
+   } 
 }
 
 document.querySelectorAll('.color-button').forEach(function(button) {
@@ -21,3 +26,28 @@ document.querySelectorAll('.color-button').forEach(function(button) {
    });
 });
 
+/*
+$(document).ready(function() {
+   $.ajax({
+      type: "GET",
+    	url: "",
+    	dataType: "json",
+   	success: function(responseData, status){
+         var output = "";
+         $.each(responseData.menuItem, function() {
+            output += ("<a href='" + this.link + "' class=\"projects\">" + this.lab + ": " + this.name + "</a>" +
+            "<p>" + this.description + "</p><br>");
+         });
+         $("#projects").css("display", "none");
+         $("#projects").html(output);
+         console.log("hi");
+         $("#projects").animate({
+             width: "toggle"
+         }, 1500);
+         }, error: function(msg) {
+         // there was a problem
+         alert("There was a problem: " + msg.status + " " + msg.statusText);
+    	}
+    })
+})
+*/
