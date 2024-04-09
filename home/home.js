8cd9c80e615a20988fd6885eaaed349be0fc82b6
jordyny <jordyn.young2234@gmail.com>
2024-04-08 20:14:14 -0400

    updated developers
    added profile images and names to home page

---
+++ home/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#hideRSS").click(function() {
+        $("#lab9RSS").fadeOut(500);
+      });
+     
+      $("#showRSS").click(function() {
+        $("#lab9RSS").fadeIn(500);
+      });
+
+
+    $("#hideAtom").click(function() {
+        $("#lab9Atom").fadeOut(500);
+      });
+     
+      $("#showAtom").click(function() {
+        $("#lab9Atom").fadeIn(500);
+    });
+});
